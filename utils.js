a3c0063b891a401df571f076bc9efe7f857cef23
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-09 18:44:01 -0500

    add get location

---
+++ utils.js
@@ -0,0 +1,11 @@
+function mungedLocation(location) {
+  return {
+    formatted_query: location[0].display_name,
+    latitude: location[0].lat,
+    longitude: location[0].lon
+  };
+}
+
+module.exports = {
+  mungedLocation
+};
