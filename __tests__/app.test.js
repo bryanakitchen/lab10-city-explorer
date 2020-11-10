323d790e48323a08d7d32781c46683d9612a4fec
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2020-11-09 21:02:48 -0500

    minor edit

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,36 +1,9 @@
 require('dotenv').config();
 
-const { execSync } = require('child_process');
-
-const fakeRequest = require('supertest');
-const app = require('../lib/app');
-const client = require('../lib/client');
 const { mungedLocation } = require('../utils');
 
 describe('app routes', () => {
   describe('routes', () => {
-    let token;
-  
-    beforeAll(async done => {
-      execSync('npm run setup-db');
-  
-      client.connect();
-  
-      const signInData = await fakeRequest(app)
-        .post('/auth/signup')
-        .send({
-          email: 'jon@user.com',
-          password: '1234'
-        });
-      
-      token = signInData.body.token;
-  
-      return done();
-    });
-  
-    afterAll(done => {
-      return client.end(done);
-    });
 
     test('returns munged location', async() => {
 
